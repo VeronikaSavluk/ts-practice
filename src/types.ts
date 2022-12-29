915821f503ab909342111062f7c4f7ceeff1fb32
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2022-12-30 00:01:30 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -10,3 +10,18 @@ getPromise ()
   console.log(data);
 });
 
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number
+};
+
+function compare (top: Pick<AllType, 'name' | 'color'>, bottom: Pick<AllType, 'position' | 'weight'>): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  }
+}
