9ba0de87f23a7d651ca63c30df1649782237668f
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2022-12-29 23:56:34 +0200

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+function getPromise (): Promise<Array<string | number>> {
+  return new Promise((resolve) => {
+    resolve(['Text', 50]);
+  });
+}
+
+
+getPromise ()
+.then((data) => {
+  console.log(data);
+});
+
